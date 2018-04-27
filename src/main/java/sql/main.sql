0194e9192ac6f25d59de92407ff557eaa676a8ad
BauyrzhanMaksot <bauyrzhan.maksot@nu.edu.kz>
2018-04-27 12:30:46 +0600

    mysql added(fix)

--- src/main/java/sql/main.sql
+++ src/main/java/sql/main.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `roles`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `contactNumber` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
